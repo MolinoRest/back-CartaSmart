3d89639611f135db94c158c80d0011550784113e
leonardo <u202124304@upc.edu.pe>
2025-12-01 16:10:28 -0500

    add tts and stt

--- app/main.py
+++ app/main.py
@@ -9,6 +9,22 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="CartaSmart API")
 
+origins = [
+    "http://localhost:3000",
+    "http://localhost",
+    "https://front-carta-smart-production.up.railway.app",   # tu FRONT en Railway
+    "https://carta-smart-api-31496243302.europe-west1.run.app",  # tu BACK en Cloud Run
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(restaurants.router)
 app.include_router(menu_items.router)
 app.include_router(menu_categories.router)
